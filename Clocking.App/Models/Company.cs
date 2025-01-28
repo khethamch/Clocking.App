f66c4b7d9a4219b60426cdd9783b30864a8b0cf5
KhethaDev <KhethaDev@MUNBICOZA>
2025-01-28 07:24:02 +0000

    Added company and department.

---
+++ Clocking.App/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clocking.App.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RegistrationNumber { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
