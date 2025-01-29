643de376a971008adf8fa9903ef3239c274df9b9
KhethaDev <KhethaDev@MUNBICOZA>
2025-01-29 16:14:45 +0000

    Added the ICompanyRepository repository.

---
+++ Clocking.App/Repository/CompanyRepository.cs
@@ -0,0 +1,37 @@
+﻿using Clocking.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Clocking.App.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        public Task CreateCompany(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetCompanies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCompany(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Clocking.App/Repository/ICompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using Clocking.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clocking.App.Repository
+{
+    public interface ICompanyRepository
+    {
+        Task CreateCompany(Company company);
+        Task UpdateCompany(Company company);
+        Task DeleteCompany(int companyId);
+        Task GetCompany(int companyId);
+        Task GetCompanies();
+    }
+}
